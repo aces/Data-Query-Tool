14268db828e1e0dbb6f2e796f678417ad16d26a9
Dave MacFarlane <driusan@gmail.com>
2014-04-25 10:47:44 -0400

    Don't list runlog in category list

--- views/categories/map.js
+++ views/categories/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-    if(doc.Meta && doc.Meta.DocType && doc.Meta.DocType !== 'SavedQuery' && doc.Meta.DocType !== 'ServerConfig') {
+    if(doc.Meta && doc.Meta.DocType && doc.Meta.DocType !== 'SavedQuery' && doc.Meta.DocType !== 'ServerConfig' && doc.Meta.DocType !== 'RunLog') {
         emit(doc.Meta.DocType, null);
     }
 };
