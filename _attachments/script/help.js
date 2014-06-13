70c7671f6313ef7340c5dcc1f2aa47f5116cb2e1
Tara Campbell <tara0campbell@gmail.com>
2014-06-13 17:10:24 -0400

    Add title, url, back button, etc. to the help pop-up.

--- _attachments/script/help.js
+++ _attachments/script/help.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     	// Grab the ID of the currently visible panel
     	var activePanel = $('#tabs .ui-tabs-panel[aria-hidden="false"]').prop('id');
 		var url = "help/" + activePanel + ".html";
-		window.open(url, "Help", 'height=400,width=800,left=10,top=10,titlebar=no,toolbar=no,menubar=no,location=no,directories=no,status=no'); 
+		window.open(url, "Help", 'height=400,width=800,left=10,top=10,titlebar=yes,toolbar=no,menubar=no,location=yes,directories=no,status=no'); 
 	});
 
     // Add question mark icon to the help button
